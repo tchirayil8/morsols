8da5eb85ab24e1184f89abbf5f923c1af12df69d
Thomas Chirayil <tchirayil8@gmail.com>
2020-11-08 16:49:14 -0600

    WIP

--- src/containers/Homepage.jsx
+++ src/containers/Homepage.jsx
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const query = `
+{
+  homepageCollection {
+    items {
+      headerText
+      description1
+      description2
+      description3
+      description4
+    }
+  }
+}
+`
 
 export const Homepage = () => {
-    return (
-        <div className="homepage">
-            <div className="homepage__header-picture" />
-            <div className="homepage__body">
-                <div className="container">
-                    <div className="homepage__description-1">
-                        We have the Morsol to deliver the gifts of hemp, sun, and good soil to you for your enjoyment. There is so much more to hemp than that which "gets you high". You can rest easy knowing our broad spectrum hemp oil is derived from sustainably farmed hemp and the remainder of the plant can be used in many other industries.
-                    </div>
-                    <div className="homepage__description-2">
-                        We believe that outdoor grown, minimally processed hemp is the most wholesome. We have the test results, but only you can be the judge of what goodness feels like.
-                    </div>
-                </div>
-                <div className="homepage__picture" />
-                <div className="container">
-                    <div className="homepage__description-3">
-                        Morsols are designed to be used on the go: at work or school, between errands and exercise. Bring light to your life wherever and whenever you desire. A range of dosages gives you the power to choose what works for you.
-                    </div>
-                    <div className="homepage__description-4">
-                        Processing:<br />
-                        Raw hemp material is extracted woth CO2, retaining the delicate terpenes and flavonoids and leaving behind zero solvent. This is the same extraction method used in the decaffenation process for tea and coffee. The oil is then distilled and separated via column chromatography to ensure hemp compliance, an oil free of hard-to-dgiest waxes, and to maximize consumer benefits via adjusting natural ratios of cannabinoids.
-                    </div>
-                </div>
-            </div>
+  const [page, setPage] = useState(null);
+  
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/ts1x993q12de/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer 4jAC4tc_Z1Q7CMxOYCpeRgl9ZUhj2qhXBh28y3eqmoU",
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+        setPage(data.homepageCollection.items[0]);
+        });
+    }, []);
+  
+  if (!page) {
+      return "Loading...";
+  }
+
+  console.log('page',page);
+    
+  return (
+    <div className="homepage">
+      <div className="homepage__header-picture" />
+      <div className="homepage__body">
+        <div className="container">
+          <div className="homepage__description-1">
+            {page.description1}
+          </div>
+          <div className="homepage__description-2">
+            {page.description2}
+          </div>
         </div>
-    );
+        <div className="homepage__picture" />
+        <div className="container">
+          <div className="homepage__description-3">
+            {page.description3}
+          </div>
+          <div className="homepage__description-4">
+            {page.description4}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
